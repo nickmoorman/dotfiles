95f6c331d90ed712a1fb5b8755337126903ec775
Nick Sawyer <nick@nicksawyer.net>
2021-08-17 08:43:00 -0700

    Add tmux to setup script

--- setup.sh
+++ setup.sh
@@ -18,6 +18,13 @@ ln -s ${CUR_DIR}/vim/conf/colors.vim ${HOME}/.vim/conf/colors.vim
 ln -s ${CUR_DIR}/vim/conf/nerdtree.vim ${HOME}/.vim/conf/nerdtree.vim
 ln -s ${CUR_DIR}/vim/conf/vundle.vim ${HOME}/.vim/conf/vundle.vim
 
+# tmux
+mkdir -p ${HOME}/.tmux
+ln -s ${CUR_DIR}/tmux/tmux.conf ${HOME}/.tmux.conf
+ln -s ${CUR_DIR}/tmux/conf/base.conf ${HOME}/.tmux/base.conf
+ln -s ${CUR_DIR}/tmux/conf/screen-bindings.conf ${HOME}/.tmux/screen-bindings.conf
+ln -s ${CUR_DIR}/tmux/conf/tpm.conf ${HOME}/.tmux/tpm.conf
+
 # TODO: ack
 
 ln -s ${CUR_DIR}/curlrc ${HOME}/.curlrc
@@ -25,6 +32,4 @@ ln -s ${CUR_DIR}/curlrc ${HOME}/.curlrc
 ln -s ${CUR_DIR}/gitconfig ${HOME}/.gitconfig
 
 ln -s ${CUR_DIR}/rsync-excludes ${HOME}/.rsync-excludes
-
-# TODO: tmux
 
