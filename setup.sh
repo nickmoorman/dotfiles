09be0a2641703a9ffb7a791868f4ce7e255f7b07
Nick Sawyer <nick@nicksawyer.net>
2025-06-11 09:19:34 -0700

    Print informational output during setup

--- setup.sh
+++ setup.sh
@@ -14,22 +14,26 @@ for f in .bashrc .bash_profile .vimrc .tmux.conf .curlrc .gitconfig .rsync-exclu
 done
 
 # Bash
+echo "Setting up Bash configs..."
 mkdir -p ${HOME}/.bash/history
 ln -s ${CUR_DIR}/bash/bashrc ${HOME}/.bashrc
 ln -s ${CUR_DIR}/bash/bash_profile ${HOME}/.bash_profile
 ln -s ${CUR_DIR}/bash/prompt ${HOME}/.bash/prompt
 
 # Vim
+echo "Setting up Vim configs..."
 mkdir -p ${HOME}/.vim/conf
 ln -s ${CUR_DIR}/vim/vimrc ${HOME}/.vimrc
 ln -s ${CUR_DIR}/vim/conf/base.vim ${HOME}/.vim/conf/base.vim
 ln -s ${CUR_DIR}/vim/conf/colors.vim ${HOME}/.vim/conf/colors.vim
 ln -s ${CUR_DIR}/vim/conf/nerdtree.vim ${HOME}/.vim/conf/nerdtree.vim
 ln -s ${CUR_DIR}/vim/conf/vundle.vim ${HOME}/.vim/conf/vundle.vim
+echo "Cloning Vundle and installing plugins..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 # tmux
+echo "Setting up tmux configs..."
 mkdir -p ${HOME}/.tmux/conf
 ln -s ${CUR_DIR}/tmux/tmux.conf ${HOME}/.tmux.conf
 ln -s ${CUR_DIR}/tmux/conf/base.conf ${HOME}/.tmux/conf/base.conf
@@ -37,10 +41,13 @@ ln -s ${CUR_DIR}/tmux/conf/screen-bindings.conf ${HOME}/.tmux/conf/screen-bindin
 ln -s ${CUR_DIR}/tmux/conf/tpm.conf ${HOME}/.tmux/conf/tpm.conf
 # See: https://superuser.com/a/454827
 #brew install reattach-to-user-namespace
+echo "Cloning tmux-plugins..."
 # See: https://github.com/tmux-plugins/tpm/issues/67#issuecomment-464260747
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # TODO: ack
+
+echo "Symlinking .curlrc, .gitconfig, and .rsync-excludes..."
 
 ln -s ${CUR_DIR}/curlrc ${HOME}/.curlrc
 
