2f47aba3c6167c114f29ec184f00dce87c4769a6
Nick Sawyer <nick@nicksawyer.net>
2025-07-23 13:30:53 -0700

    First stab at zsh configs

--- setup_shells.sh
+++ setup_shells.sh
@@ -6,7 +6,7 @@ CUR_DIR=$(pwd)
 # TODO: Check relevant subdirectories too
 BKUPS=${HOME}/.dotfiles_backups
 mkdir -p ${BKUPS}
-for f in .bashrc .bash_profile; do
+for f in .bashrc .bash_profile .zshrc .zprofile; do
     if [[ -e ${HOME}/${f} ]]; then
         echo "Moving ${HOME}/${f} to ${BKUPS}..."
         mv ${HOME}/${f} ${BKUPS}/${f}
@@ -19,4 +19,11 @@ mkdir -p ${HOME}/.bash/history
 ln -s ${CUR_DIR}/bash/bashrc ${HOME}/.bashrc
 ln -s ${CUR_DIR}/bash/bash_profile ${HOME}/.bash_profile
 ln -s ${CUR_DIR}/bash/prompt ${HOME}/.bash/prompt
+
+# zsh
+echo "Setting up zsh configs..."
+mkdir -p ${HOME}/.zsh/history
+ln -s ${CUR_DIR}/zsh/zshrc ${HOME}/.zshrc
+ln -s ${CUR_DIR}/zsh/zprofile ${HOME}/.zprofile
+ln -s ${CUR_DIR}/zsh/prompt ${HOME}/.zsh/prompt
 
